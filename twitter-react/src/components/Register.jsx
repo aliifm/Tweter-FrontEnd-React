1949ace71d0d3cd2917d5ae5c701e7a07be9780d
felipejarodich27 <felipejarodich12@gmail.com>
2023-03-16 09:20:12 -0300

    Update Register.jsx
    Register changes

--- twitter-react/src/components/Register.jsx
+++ twitter-react/src/components/Register.jsx
@@ -34,42 +34,104 @@ function Register() {
 
   return (
     <body className="background-color-login">
-      <section className="container vh-100 d-flex justify-content-center align-items-center my-auto">
-        <div className="row">
+      <section className="container d-flex justify-content-center align-items-center my-auto">
+        <div className="row ">
           <div className="hidden-md col-md-7 bgTweeter text-primary d-flex flex-column rounded-start">
             <i className="fa-brands fa-twitter fs-1 p-4 text-white"></i>
-            <p className="text-white fs-4 mt-auto p-4">Hey! Nice to see you again🥰</p>
+            <p className="text-white fs-4 mt-auto p-4">
+              Hey! Nice to see you again🥰
+            </p>
           </div>
           <div className="col-md-5 col-12 bg-white p-5 rounded-end flex-column my-auto h-100 login-style">
             <h4>Sign up</h4>
             <p>Create an account and start using Twitter</p>
-            <form className="mb-5" action="/registeruser" method="post" enctype="multipart/form-data" onSubmit={handleSubmit}>
+            <form
+              className="mb-5"
+              action="/registeruser"
+              method="post"
+              enctype="multipart/form-data"
+              onSubmit={handleSubmit}
+            >
               <div className="mb-3">
-                <input type="text" name="firstname" placeholder="First name" className="form-control" id="firstname" aria-describedby="emailHelp" value={inputFirstname} onChange={(e) => setInputFirstname(e.target.value)} />
+                <input
+                  type="text"
+                  name="firstname"
+                  placeholder="First name"
+                  className="form-control"
+                  id="firstname"
+                  aria-describedby="emailHelp"
+                  value={inputFirstname}
+                  onChange={(e) => setInputFirstname(e.target.value)}
+                />
               </div>
               <div className="mb-3">
-                <input type="text" className="form-control" name="lastname" placeholder="Last name" id="lastname" value={inputLastname} onChange={(e) => setInputLastname(e.target.value)} />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="lastname"
+                  placeholder="Last name"
+                  id="lastname"
+                  value={inputLastname}
+                  onChange={(e) => setInputLastname(e.target.value)}
+                />
               </div>
               <div className="mb-3">
-                <input type="email" className="form-control" name="email" placeholder="Email" id="email" value={inputEmail} onChange={(e) => setInputEmail(e.target.value)} />
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  placeholder="Email"
+                  id="email"
+                  value={inputEmail}
+                  onChange={(e) => setInputEmail(e.target.value)}
+                />
               </div>
               <div className="mb-3">
-                <input type="text" className="form-control" name="username" placeholder="Username" id="username" value={inputUsername} onChange={(e) => setInputUsername(e.target.value)} />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="username"
+                  placeholder="Username"
+                  id="username"
+                  value={inputUsername}
+                  onChange={(e) => setInputUsername(e.target.value)}
+                />
               </div>
               <div className="mb-3">
-                <input type="password" className="form-control" name="password" placeholder="Password" id="password" value={inputPassword} onChange={(e) => setInputPassword(e.target.value)} />
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  placeholder="Password"
+                  id="password"
+                  value={inputPassword}
+                  onChange={(e) => setInputPassword(e.target.value)}
+                />
               </div>
               <div className="mb-3">
                 <label for="image" className="form-label">
                   Seleccionar una imagen
                 </label>
-                <input type="file" className="form-control" id="avatar" name="avatar" value={inputAvatar} onChange={(e) => setInputAvatar(e.target.value)} />
+                <input
+                  type="file"
+                  className="form-control"
+                  id="avatar"
+                  name="avatar"
+                  value={inputAvatar}
+                  onChange={(e) => setInputAvatar(e.target.value)}
+                />
               </div>
-              <button type="submit" className="btn bgTweeter rounded-pill w-100">
+              <button
+                type="submit"
+                className="btn bgTweeter rounded-pill w-100"
+              >
                 Sign Up
               </button>
             </form>
-            <a className="text-decoration-none text-dark text-center d-block " href="/">
+            <a
+              className="text-decoration-none text-dark text-center d-block "
+              href="/"
+            >
               Already have an account?
               <Link to="/"> Sign in </Link>
             </a>
