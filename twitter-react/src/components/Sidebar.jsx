7a911f255fea068a8eb69f4b75ebbe57dab98227
felipejarodich27 <felipejarodich12@gmail.com>
2023-03-17 15:53:29 -0300

    Vista register, url en home, etc

--- twitter-react/src/components/Sidebar.jsx
+++ twitter-react/src/components/Sidebar.jsx
@@ -9,28 +9,58 @@ import { useSelector } from "react-redux";
 function Sidebar() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user.user);
+
   const navigate = useNavigate();
 
   return (
     <div className="col-sm-auto sticky-top vh-100">
       <div className="d-flex flex-column align-items-center sticky-top h-100">
-        <Link to={"/"} className="d-block p-3 link-primary text-decoration-none fs-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Icon-only">
+        <Link
+          to={"/"}
+          className="d-block p-3 link-primary text-decoration-none fs-2"
+          title=""
+          data-bs-toggle="tooltip"
+          data-bs-placement="right"
+          data-bs-original-title="Icon-only"
+        >
           <i className="fa-brands fa-twitter"></i>
         </Link>
         <ul className="nav nav-pills nav-flush flex-column mb-auto mx-auto text-center align-items-center">
           <li className="nav-item">
-            <Link to={"/"} className="d-block nav-link py-3 px-2 link-dark fs-3" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
+            <Link
+              to={"/"}
+              className="d-block nav-link py-3 px-2 link-dark fs-3"
+              title=""
+              data-bs-toggle="tooltip"
+              data-bs-placement="right"
+              data-bs-original-title="Home"
+            >
               <i className="fa-solid fa-house"></i>
             </Link>
           </li>
           <li>
-            <Link to={`/profile/${user.username}`} className="d-block nav-link py-3 px-2 link-dark fs-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
+            <Link
+              to={`/profile/${user.username}`}
+              className="d-block nav-link py-3 px-2 link-dark fs-2"
+              title=""
+              data-bs-toggle="tooltip"
+              data-bs-placement="right"
+              data-bs-original-title="Dashboard"
+            >
               <i className="fa-regular fa-user"></i>
             </Link>
           </li>
           <li>
-            <a href="/" className="d-block nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Orders">
+            <a
+              href="/"
+              className="d-block nav-link py-3 px-2"
+              title=""
+              data-bs-toggle="tooltip"
+              data-bs-placement="right"
+              data-bs-original-title="Orders"
+            >
               <span className="btn bgTweeter rounded-pill">Tweet</span>
+              <i></i>
             </a>
           </li>
         </ul>
